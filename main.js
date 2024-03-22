089bb7821dbbcb352165ab163aaec99f9c7b95db
umair-jami <lalaumair92@gmail.com>
2024-03-23 01:14:43 +0500

    Numnber Gussing

---
+++ main.js
@@ -0,0 +1,25 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+//01) computer will generate random number
+// 2)take input from user
+// 3)condition check kerna
+const randomNumber = Math.floor(Math.random() * 21);
+const answers = await inquirer.prompt([
+    {
+        name: "userGuessNumber",
+        type: "number",
+        message: "Please Guess Number:"
+    }
+]);
+if (answers.userGuessNumber === randomNumber) {
+    console.log("Your Guess is Right");
+}
+else if (answers.userGuessNumber > randomNumber) {
+    console.log("Your guess is  too high");
+}
+else if (answers.userGuessNumber < randomNumber) {
+    console.log("Your guess is  too low");
+}
+else {
+    console.log("Your guess is wrong");
+}
